1480008d5956531a08655dbcb87ad2a9e39d3590
Kamil Brdy <kamil.brdys@gmail.com>
2020-06-05 17:45:10 +0100

    Frontend

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react'
+import { IActivity } from '../models/activity';
+import Axios from 'axios';
+import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectedActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0])
+    setEditMode(false);
+  }
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  }
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
+
+  useEffect(() => {
+    let source = axios.CancelToken.source();
+    try {
+      axios
+        .get<IActivity[]>('http://localhost:5000/api/activities', {
+          cancelToken: source.token
+        })
+        .then(response => {
+          let activities: IActivity [] = [];
+          response.data.forEach (activity => {
+            activity.date = activity.date.split('.')[0];
+            activities.push(activity);
+          })
+          setActivities(activities)
+        });
+    }
+    catch (error) {
+      if (Axios.isCancel(error)) {
+        console.log("Request cancelled");
+      } else {
+        throw error;
+      }
+    }
+    return () => {
+      console.log('Unmounting');
+      source.cancel();
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectedActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+}
+
+
+export default App;
